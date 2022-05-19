73c791d85200ca25537909859528a8451874839d
jeongdxxn <dxxn.jeong@gmail.com>
2022-05-19 23:18:04 +0900

    frontend setup

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
-import express from "express";
+const express = require("express");
 
 const app = express();
 
-console.log("hello");
+app.set("view engine", "pug");
+app.set("view", __dirname + "/src/views");
+app.get("/", (req, res) => res.render("home"));
 
-app.listen(3000);
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+console.log("hello", handleListen);
+
+let server = app.listen(3000);
+
+module.exports = server;
