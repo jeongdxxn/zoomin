deafe864e5b10df82333de72ada1fc4f0da27196
jeongdxxn <dxxn.jeong@gmail.com>
2022-05-20 23:58:36 +0900

    frontend setup

--- src/server.js
+++ src/server.js
@@ -2,13 +2,14 @@ const express = require("express");
 
 const app = express();
 
+// pug를 view engine으로 설정
 app.set("view engine", "pug");
-app.set("view", __dirname + "/src/views");
+// express에 template가 어디에 있는지 지정
+app.set("views", __dirname + "/views");
+// public url을 생성해서 유저에게 파일 공유
+app.use("/public", express.static(__dirname + "/public"));
+// home.pug를 render하는 route handler 생성
 app.get("/", (req, res) => res.render("home"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-console.log("hello", handleListen);
-
-let server = app.listen(3000);
-
-module.exports = server;
+app.listen(3000, handleListen);
